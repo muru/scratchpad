b2f216f741adf4e35e2d981e669da268e2bf6574
Murukesh Mohanan <murukesh.mohanan@gmail.com>
2013-09-05 08:05:46 +0530

    Started expressions.

--- haskell/MicroHaskell.hs
+++ haskell/MicroHaskell.hs
@@ -1,6 +1,7 @@
 module MicroHaskell where
 
 import FParser
+import Data.Char
 
 type Fname = String
 type Var = String
@@ -12,19 +13,43 @@ data Exp		= I Int | V Var | B Bool | MNil |
 				  App Exp Exp deriving Show
 				  
 
--- intlit :: [Char] -> Exp
+type ExpParser = Parser Char Exp
+
+name = (satisfy isStartWord) <.> (satisfy (isWord) <*) 
+
+intlit :: ExpParser
 intlit = integer <@ I
 
--- boollit :: Exp
-boollit = ((token "True" <|> token "False") <@ f)
-		  where f x | x == "True"	= B True
+boollit :: ExpParser
+boollit	= ((token "True" <|> token "False") <@ f)
+		where f x | x == "True"	= B True
 					| x == "False"	= B False
 
--- nil :: [Char] ->  Exp
-nil = token "[]" <@ f
-	  where f "[]" = MNil
+mnil :: ExpParser
+mnil	= token "[]" <@ f
+		where f "[]" = MNil
 
+isWord x = isAlphaNum x || (x ==  '_')
+isStartWord x = isAlpha x || (x ==  '_')
+var :: ExpParser
+var = name <@ (\ (x, y) ->  V (x:y))
 
+fname :: ExpParser
+fname = name <@ (\ (x, y) -> Fname (x:y))
+
+constant = intlit <|> boollit <|> mnil
+
+operator x = token x <@ op
+			where	op "+" = (+)
+					op "-" = (-)
+
+expression	= constant <|> var
+			<|> (expression <.> token "+" <.> expression) <@ optofname
+			<|> (expression <.> token "-" <.> expression)
+			<|> (fname <.> (expression <*))
+			<|> (expression <.> token "==" <.> expression)
+			where optofname (x,(y,z)) =  App (Fname y) (App x z)
+			
 
 
 
