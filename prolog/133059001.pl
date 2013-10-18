217607e00202e21b7460b54a4d476c6dfa2a6d14
Murukesh Mohanan <murukesh.mohanan@gmail.com>
2013-10-18 10:02:46 +0530

    AI quiz, Q1 - seems to work

--- prolog/133059001.pl
+++ prolog/133059001.pl
@@ -15,13 +15,20 @@
 %powerSet(S,Ans):-false.
 powerSet([],[[]]).
 powerSet([X|Y],S) :- var(S), powerSet(Y,SY), extend(X,SY,SX), append(SY,SX,S).
-powerSet(X,S) :- nonvar(S), maplist(subsetof(X), S).
+powerSet(X,S) :- nonvar(S), powerSet(X,SX), equivalent_sset(S, SX).
 
 extend(X,[Y], [[X|Y]]).
 extend(X,[Y|YS],S) :- extend(X,YS,SX), append([[X|Y]], SX, S).
 
-subsetof(_,[]).
-subsetof(S,[X|XS]) :- member(X,S), subsetof(S,XS).
+equivalent_sset(SX,SY) :- maplist(memberof(SY), SX), maplist(memberof(SX), SY).
+
+equivalent_set([],[]).
+equivalent_set(X,Y) :- maplist(contains(Y), X), maplist(contains(X), Y).
+
+contains(Y, X) :- member(X, Y).
+
+memberof(Y, X) :- member(X, Y).
+memberof(Y, [X|XS]) :- member(Z, Y), equivalent_set(Z, [X|XS]).
 
 % Write the predicates for Problem 2 below:
 % Do not change the "name" of the predicate given below
