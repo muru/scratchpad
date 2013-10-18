3e70f819c68a59c60483c302728c4590a044916f
Murukesh Mohanan <murukesh.mohanan@gmail.com>
2013-10-18 10:41:40 +0530

    AI quiz, submitted.

--- prolog/133059001.pl
+++ prolog/133059001.pl
@@ -15,32 +15,28 @@
 %powerSet(S,Ans):-false.
 powerSet([],[[]]).
 powerSet([X|Y],S) :- var(S), powerSet(Y,SY), extend(X,SY,SX), append(SY,SX,S).
-powerSet(X,S) :- nonvar(S), powerSet(X,SX), equivalent_sset(S, SX).
+powerSet(X,S) :- nonvar(S), powerSet(X,SX), equivalent_set(S, SX).
 
 extend(X,[Y], [[X|Y]]).
 extend(X,[Y|YS],S) :- extend(X,YS,SX), append([[X|Y]], SX, S).
 
-equivalent_sset(SX,SY) :- maplist(memberof(SY), SX), maplist(memberof(SX), SY).
-
 equivalent_set([],[]).
 equivalent_set(X,Y) :- maplist(contains(Y), X), maplist(contains(X), Y).
 
-contains(Y, X) :- member(X, Y).
+contains(Y, X) :- is_list(X), member(Z, Y), equivalent_set(Z, X).
+contains(Y, X) :- not(is_list(X)), member(X, Y).
 
-memberof(Y, X) :- member(X, Y).
-memberof(Y, [X|XS]) :- member(Z, Y), equivalent_set(Z, [X|XS]).
+is_list([]).
+is_list([_|XS]) :- is_list(XS).
 
 % Write the predicates for Problem 2 below:
 % Do not change the "name" of the predicate given below
 % Your assignment will not be evaluated otherwise
 
-%flatten(In,Out):-false.
-flatten([],[]).
+%flatten2(In,Out):-false.
 
-
-
-
-
-
+flatten2([],[]).
+flatten2([X|Y],Z) :- is_list(X), flatten2(X, L1), flatten2(Y, L2), append(L1, L2, Z).
+flatten2([X|Y],[X|L]) :- not(is_list(X)), flatten2(Y, L).
 
 % End of file
