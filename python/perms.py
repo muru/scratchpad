981fffba62dd7c8039732432dbda3a73c16bbf21
Murukesh Mohanan <murukesh.mohanan@gmail.com>
2014-11-27 06:41:06 +0530

    Added permutations

--- python/perms.py
+++ python/perms.py
@@ -6,17 +6,18 @@ import itertools
 
 def usage (exit_code = 0):
     print ('Usage:')
-    print (sys.argv[0] + ' [--help] [-x|--max-depth=N] [-n|--min-depth=M] [file]')
+    print (sys.argv[0] + ' [--help] [-x|--max-depth=N] [-n|--min-depth=M] [-r|--repeat] [file]')
     sys.exit (exit_code)
 
 try:
-    optlist, args = getopt.gnu_getopt (sys.argv, 'x:n:h', ['max-depth=', 'min-depth=', '--help'])
+    optlist, args = getopt.gnu_getopt (sys.argv, 'x:n:hr', ['max-depth=', 'min-depth=', '--help', '--repeat'])
 except getopt.GetoptError as err:
     print (err)
     usage (2)
 
 min_depth = 1
 max_depth = 1
+repeat = False
 
 for (opt, val) in optlist:
     if opt in ('-x', '--max-depth'):
@@ -25,6 +26,13 @@ for (opt, val) in optlist:
         min_depth = int(val)
     elif opt in ('-h', '--help'):
         usage ()
+    elif opt in ('-r', '--repeat'):
+        repeat = True
+
+if repeat:
+    mix = itertools.product
+else:
+    mix = itertools.permutations
 
 if len (args) == 1:
     in_file = sys.stdin
@@ -34,6 +42,6 @@ else:
 phrases = [ line.strip () for line in in_file.readlines () ]
 
 for i in range (min_depth, max_depth + 1):
-    passwords = itertools.product (phrases, repeat=i)
+    passwords = mix (phrases, i)
     for pw in passwords:
         print (''.join(pw))
